847cad8a186dd4a2b2117d29199acc57cd326622
Surya K L <suryakannaiyan4@gmail.com>
2025-05-22 12:40:07 +0530

    Revert "Backend for QueryManagement Status-Working"
    This reverts commit 524858d65084531e7124b9df6824c72d8276657c.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const cors = require('cors');
 const compression = require('compression');
 
@@ -14,7 +15,6 @@ const errorHandlers = require('./handlers/errorHandlers');
 const erpApiRouter = require('./routes/appRoutes/appApi');
 
 const fileUpload = require('express-fileupload');
-const queryManagementRouter = require('./routes/queryManagementRouter');
 // create our Express app
 const app = express();
 
@@ -41,15 +41,12 @@ app.use('/api', adminAuth.isValidAuthToken, coreApiRouter);
 app.use('/api', adminAuth.isValidAuthToken, erpApiRouter);
 app.use('/download', coreDownloadRouter);
 app.use('/public', corePublicRouter);
-app.use('/test', queryManagementRouter);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
 
 // production error handler
 app.use(errorHandlers.productionErrors);
-// app.use('/api', adminAuth.isValidAuthToken, queryManagementRouter);
-
 
 // done! we export it so we can start the site in start.js
 module.exports = app;
